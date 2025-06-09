ddcd3f72e007c5bc00ea8543f4341a803cbf5e46
figarofuga <61215157+figarofuga@users.noreply.github.com>
2025-06-09 13:04:37 +0000

    add grf

--- test.R
+++ test.R
@@ -6,17 +6,28 @@ library(tmle)
 
 rhc <- data.table::fread(here::here("rhc.csv")) |> 
     dplyr::mutate(
-        swang1 = if_else(swang1 == "RHC", 1, 0)
-    )
+        swang1 = if_else(swang1 == "RHC", 1, 0), 
+        death01 = if_else(death == "Yes", 1, 0)
+    ) |> 
+    tidyr::drop_na(death01, swang1, age, crea1, alb1, hrt1)
 
+Y <- rhc$death01
+A <- rhc$swang1
+W <- rhc[, c("age", "crea1", "alb1", "hrt1")]
 
 
 tmle::tmle(
-  Y = rhc$y,
-  A = rhc$A,
-  W = rhc[, c("x1", "x2", "x3")],
+  Y,
+  A,
+  W,
   family = "binomial",
   Q.SL.library = c("SL.mean"),
   g.SL.library = c("SL.mean"),
   verbose = TRUE
+)
+
+fit <- grf::quantile_forest(
+  X = W,
+  Y = rhc$hema1,
+  quantiles = c(0.25, 0.5, 0.75)
 )
