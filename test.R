14bab3f423fa42e331df9fe757e629963a2fd41f
figarofuga <61215157+figarofuga@users.noreply.github.com>
2025-10-16 03:10:33 +0000

    update

--- test.R
+++ test.R
@@ -1,41 +1,26 @@
 library(here)
 library(data.table)
+library(tinyplot)
+library(parameters)
+
 library(rms)
-library(tidyverse)
 library(grf)
 library(tmle)
 library(SuperLearner)
-
-rhc <- data.table::fread(here::here("rhc.csv")) |> 
-    dplyr::mutate(
-        swang1 = if_else(swang1 == "RHC", 1, 0), 
-        death01 = if_else(death == "Yes", 1, 0)
-    ) |> 
-    tidyr::drop_na(death01, swang1, age, crea1, alb1, hrt1)
-
-Y <- rhc$death01
-A <- rhc$swang1
-W <- rhc[, c("age", "crea1", "alb1", "hrt1")]
+library(WeightIt)
+library(marginaleffects)
+library(MatchIt)
 
 
-tmle::tmle(
-  Y,
-  A,
-  W,
-  family = "binomial",
-  Q.SL.library = c("SL.mean"),
-  g.SL.library = c("SL.mean"),
-  verbose = TRUE
-)
+lalonde <- setDT(MatchIt::lalonde)
 
-fit <- grf::quantile_forest(
-  X = W,
-  Y = rhc$hema1,
-  quantiles = c(0.25, 0.5, 0.75)
-)
+ols_fit <- rms::ols(re78 ~ treat + age + educ + race + married + nodegree + re74 + re75, data = lalonde)
 
-tinyplot::plt(
-  alb1 ~ hema1 | sex, data = rhc, alpha = 0.5, 
-  xlab = "ヘマトクリット", ylab = "アルブミン"
 
-)
+# rhc <- data.table::fread(here::here("rhc.csv")) |> 
+#     dplyr::mutate(
+#         swang1 = if_else(swang1 == "RHC", 1, 0), 
+#         death01 = if_else(death == "Yes", 1, 0)
+#     ) |> 
+#     tidyr::drop_na(death01, swang1, age, crea1, alb1, hrt1)
+
